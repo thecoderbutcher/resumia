4d8ef6984b58e0b41ff02c8408d22f0f538a6f64
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2026-01-05 07:21:08 -0300

    update routes add home route

--- constants/routes.jsx
+++ constants/routes.jsx
@@ -1,9 +1,15 @@
+import Home from "../src/pages/Home";
 import ResumeAnalyzer  from "../src/pages/ResumeAnalyzer";
 import ResumeCreator  from "../src/pages/ResumeCreator";
 
 export const ROUTES = [
-  {
+   {
     path: "/",
+    element: <Home />,
+    label: { es: "Inicio", en: "Home" },
+  },
+  {
+    path: "/analyzer",
     element: <ResumeAnalyzer />,
     label: { es: "Analizar Cv", en: "Analyze Resume" },
   },
